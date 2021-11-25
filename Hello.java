548fbd9826e00335e9b2b6a2aa337f1108f6c069
ggabdoll <ggabdoll0327@naver.com>
2021-11-25 19:17:11 +0900

    test

--- Hello.java
+++ Hello.java
@@ -2,7 +2,10 @@ class Hello {
 
     public static void main(String[] args) {
     
-        System.out.println("Hellow world!");
+        System.out.println("Hellow world!1");
+        System.out.println("Hellow world!2");
+        System.out.println("Hellow world!3");
+        
 
     }
 
