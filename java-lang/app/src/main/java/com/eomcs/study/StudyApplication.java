6f7bbb161093e88e70a21d9a6a2675df31214f64
ggabdoll <ggabdoll0327@naver.com>
2021-11-29 16:26:42 +0900

    spring setting complete

--- java-lang/app/src/main/java/com/eomcs/study/StudyApplication.java
+++ java-lang/app/src/main/java/com/eomcs/study/StudyApplication.java
@@ -1,13 +1,22 @@
 package com.eomcs.study;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
+@EnableAutoConfiguration
 public class StudyApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(StudyApplication.class, args);
+  }
+  @RequestMapping("/hello")
+  String hello() {
+    return "Hello World!";
   }
 
 }
