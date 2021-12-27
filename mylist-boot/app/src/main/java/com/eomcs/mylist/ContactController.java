e5929726f409cc0acb0371b1e1a5be2b1488a370
ggabdoll <ggabdoll0327@naver.com>
2021-12-27 11:46:23 +0900

    Contact Class 분할

--- mylist-boot/app/src/main/java/com/eomcs/mylist/ContactController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/ContactController.java
@@ -7,97 +7,45 @@ import org.springframework.web.bind.annotation.RestController;
 //이 클래스가 클라이언트 요청 처리 담당자임을 표시한다.
 //이 표시(어노테이션)가 붙어 있어야만 스프링부트가 이 클래스를 인식한다.
 public class ContactController {
-  Contact[] contacts = new Contact[5];
-  int size = 0; 
+
 
   @RequestMapping("/contact/list")
   public Object list() {
-    Contact[] arr = new Contact[size]; //배열에 저장된 값만 복사할 새 배열을 만든다. 
-    for (int i =0; i < size; i++) {
-      arr[i] = contacts[i]; //전체 배열에서 값이 들어 있는 항목만 복사한다. 
-    }
-    return arr; //복사한 항목들을 담고 있는 새 배열을 리턴한다. 
+    return ArrayList.toArray();  
   }
 
   @RequestMapping("/contact/add")
   public Object add(Contact contact) {
-    if(size == contacts.length) {
-      contacts = grow();//메서드 이름에서 해당 코드에 대한 설명을 짐작할 수 있다. 
-    }
-    contacts[size++] = contact;
-
-    return size;
+    ArrayList.add(contact);
+    return ArrayList.size;
   }
 
   @RequestMapping("/contact/get")
   public Object get(String email) {
-    int index = indexOf(email);
+    int index = ArrayList.indexOf(email);
     if(index == -1) {
       return "";
     }
-    return contacts[index];
+    return ArrayList.contacts[index];
   }
   @RequestMapping("/contact/update")
   public Object update(Contact contact) {
-    int index = indexOf(contact.email);
+    int index = ArrayList.indexOf(contact.email);
     if (index == -1) {
       return 0;
     }
-    contacts[index] = contact;
-    return 1;
+    return ArrayList.set(index, contact) == null ? 0 : 1;
   }
   @RequestMapping("/contact/delete")
   public Object delete(String email) {
-    int index = indexOf(email);
+    int index = ArrayList.indexOf(email);
     if (index == -1) {
       return 0;
     }
-    remove(index);
+    ArrayList.remove(index);
     return 1;
   }
   //이메일 값으로 배열의 value를 찾는다. 
-  int indexOf(String email) {
-    for(int i = 0; i < size; i++) {
-      if(contacts[i].email.equals(email)) {
-        return i;
-      }
-    }
-    return -1;
-  }
-  //배열에서 지정한 항목을 삭제한다. 
-  Contact remove(int index) {
-    Contact old = contacts[index];
-    for(int i= index + 1; i < size; i++) {
-      contacts[i-1] = contacts[i];
-    }
-    size--;
-    return old;
-  }
 
-  //배열의 크기를 늘린다. 
-  //기존 배열의 값을 복사해온다.
-  Contact[] grow() {
-    //기 존 배열 보다 50% 큰 배열을 새로 만든다. 
-    Contact[] arr = new Contact[newLength()];
-    copy(contacts, arr);
-    return arr;
-  }
-  //주어진 배열에 대해 50% 증가시킨 새 배열의 길이를 알아낸다. 
-  int newLength() {
-    return contacts.length + (contacts.length>>1); 
-  }
-
-  //배열을 복사한다. 
-  void copy(Contact[] source, Contact[] target) {
-    //개발자가 잘못 하용할 것을 대비해서 다음 코드를 추가한다. 
-    //즉 target 배열이 source 배열 보다 작을 경우 target 배열 크기만큼만 복사한다. 
-    int length = source.length;
-    if(target.length < source.length) {
-      length = target.length;
-    }
-    for(int i =0; i < length; i++) {
-      target[i] = source[i];
-    }
-  }
 
 }
