fd4262a190460636a2a8f75a797cce16bd61b71c
ggabdoll <ggabdoll0327@naver.com>
2021-12-28 15:58:13 +0900

    ArrayList Refectoring

--- mylist-boot/app/src/main/java/com/eomcs/mylist/ArrayList.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/ArrayList.java
@@ -2,39 +2,30 @@ package com.eomcs.mylist;
 
 public class ArrayList {
 
-  static Contact[] contacts = new Contact[5];
+  static Object[] list = new Object[5];
   static int size = 0; 
 
-  static int indexOf(String email) {
-    for(int i = 0; i < size; i++) {
-      if(contacts[i].email.equals(email)) {
-        return i;
-      }
+  // 배열에 항목을 추가한다. 
+  // 배열이 꽉찼으면 배열의 크기를 늘린다.
+  static void add(Object obj) {
+    if(size == list.length) {
+      list = grow();//메서드 이름에서 해당 코드에 대한 설명을 짐작할 수 있다. 
     }
-    return -1;
+    list[size++] = obj;
   }
 
-  static Contact remove(int index) {
-    Contact old = contacts[index];
-    for(int i= index + 1; i < size; i++) {
-      contacts[i-1] = contacts[i];
-    }
-    size--;
-    return old;
-  }
-
-  static Contact[] grow() {
+  static Object[] grow() {
     //기 존 배열 보다 50% 큰 배열을 새로 만든다. 
-    Contact[] arr = new Contact[newLength()];
-    copy(contacts, arr);
+    Object[] arr = new Object[newLength()];
+    copy(list, arr);
     return arr;
   }
 
   static int newLength() {
-    return contacts.length + (contacts.length>>1); 
+    return list.length + (list.length>>1); 
   }
 
-  static void copy(Contact[] source, Contact[] target) {
+  static void copy(Object[] source, Object[] target) {
     //개발자가 잘못 하용할 것을 대비해서 다음 코드를 추가한다. 
     //즉 target 배열이 source 배열 보다 작을 경우 target 배열 크기만큼만 복사한다. 
     int length = source.length;
@@ -47,31 +38,32 @@ public class ArrayList {
   }
 
   // 배열에 저장된 목록만 꺼내 새 배열에 담아 리턴한다. 
-  static Contact[] toArray() {
-    Contact[] arr = new Contact[size]; //배열에 저장된 값만 복사할 새 배열을 만든다. 
+  static Object[] toArray() {
+    Object[] arr = new Object[size]; //배열에 저장된 값만 복사할 새 배열을 만든다. 
     for (int i =0; i < size; i++) {
-      arr[i] = contacts[i]; //전체 배열에서 값이 들어 있는 항목만 복사한다. 
+      arr[i] = list[i]; //전체 배열에서 값이 들어 있는 항목만 복사한다. 
     }
     return arr;
   }
 
-  // 배열에 항목을 추가한다. 
-  // 배열이 꽉찼으면 배열의 크기를 늘린다.
-  static void add(Contact contact) {
-    if(size == contacts.length) {
-      contacts = grow();//메서드 이름에서 해당 코드에 대한 설명을 짐작할 수 있다. 
+  static Object remove(int index) {
+    Object old = list[index];
+    for(int i= index + 1; i < size; i++) {
+      list[i-1] = list[i];
     }
-    contacts[size++] = contact;
+    size--;
+    return old;
   }
 
-  // 배열의 특정 위치의 값을 변경한다. 
+  //배열의 특정 위치의 값을 변경한다. 
   // 리턴 값은 변경하기 전에 저장되어 있던 값이다. 
-  static Contact set(int index, Contact contact) {
+  static Object set(int index, Object obj) {
     if (index < 0 || index >= size) { //값이 저장된 위치가 무표한 인덱스라면 
       return null;
     }
-    Contact old = contacts[index];
-    contacts[index] = contact;
+    Object old = list[index];
+    list[index] = obj;
     return old;
   }
+
 }
--- mylist-boot/app/src/main/java/com/eomcs/mylist/ContactController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/ContactController.java
@@ -22,15 +22,15 @@ public class ContactController {
 
   @RequestMapping("/contact/get")
   public Object get(String email) {
-    int index = ArrayList.indexOf(email);
+    int index = indexOf(email);
     if(index == -1) {
       return "";
     }
-    return ArrayList.contacts[index];
+    return ArrayList.list[index];
   }
   @RequestMapping("/contact/update")
   public Object update(Contact contact) {
-    int index = ArrayList.indexOf(contact.email);
+    int index = indexOf(contact.email);
     if (index == -1) {
       return 0;
     }
@@ -38,14 +38,22 @@ public class ContactController {
   }
   @RequestMapping("/contact/delete")
   public Object delete(String email) {
-    int index = ArrayList.indexOf(email);
+    int index = indexOf(email);
     if (index == -1) {
       return 0;
     }
     ArrayList.remove(index);
     return 1;
   }
-  //이메일 값으로 배열의 value를 찾는다. 
+  static int indexOf(String email) {
+    for(int i = 0; i < ArrayList.size; i++) {
+      Contact contact = (Contact) ArrayList.list[i];
+      if(contact.email.equals(email)) {
+        return i;
+      }
+    }
+    return -1;
+  } 
 
 
 }
