f4146502bd6a918b4c472511de03dfafc0aabe97
ggabdoll <ggabdoll0327@naver.com>
2022-01-06 15:54:15 +0900

    Book class

--- mylist-boot/app/src/main/java/com/eomcs/mylist/App.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/App.java
@@ -1,8 +1,12 @@
 package com.eomcs.mylist;
 
+import java.util.Arrays;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +18,23 @@ public class App {
   public static void main(String[] args) {
     SpringApplication.run(App.class, args);
   }
+
+  @Bean
+  public CommandLineRunner commandLineRunner(ApplicationContext beanContainer) {
+    return args -> {
+
+      System.out.println("Bean Container가 생성한 Object(Bean Container에 들어있은 Object)");
+
+      String[] beanNames = beanContainer.getBeanDefinitionNames();
+      Arrays.sort(beanNames);
+      for (int i = 0; i < beanNames.length; i++) {
+        Object bean = beanContainer.getBean(beanNames[i]);
+        System.out.printf("---->%03d: %s\n", i+1, bean.getClass().getName());
+      }
+
+    };
+  }
+
   @RequestMapping("/hello")
   String hello() {
     return "Hello World!";
