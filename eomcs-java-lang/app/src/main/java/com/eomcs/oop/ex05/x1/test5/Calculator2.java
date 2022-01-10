bcd6c1d3df5d94496ef704649575a9debba1bbc4
ggabdoll <ggabdoll0327@naver.com>
2022-01-10 18:37:20 +0900

    extend & delegation

--- eomcs-java-lang/app/src/main/java/com/eomcs/oop/ex05/x1/test5/Calculator2.java
+++ eomcs-java-lang/app/src/main/java/com/eomcs/oop/ex05/x1/test5/Calculator2.java
@@ -1,23 +1,11 @@
 package com.eomcs.oop.ex05.x1.test5;
 
-public class Calculator2 {
-  // plus(), minus()는 기존의 Calculator 클래스에게 위임한다. 
-  com.eomcs.oop.ex05.x1.test.Calculator origin = new com.eomcs.oop.ex05.x1.test.Calculator();
+import com.eomcs.oop.ex05.x1.test.Calculator;
 
-  public void plus(int value) {
-
-    origin.plus(value);
-  }
-
-  public void minus(int value) {
-    origin.minus(value);
-  }
+public class Calculator2 extends Calculator{
 
   public void multiple(int value) {
-    origin.result *= value;
+    this.result *= value;
   }
 
-  public int getResult() {
-    return origin.result;
-  }
 }
