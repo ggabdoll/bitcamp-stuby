5c3e740ae4cfa215dbfb0c93f8e4636c5d5cd445
ggabdoll <ggabdoll0327@naver.com>
2022-01-12 17:57:24 +0900

    abstract final overiding

--- eomcs-java-lang/app/src/main/java/com/eomcs/oop/ex06/d/Exam0110.java
+++ eomcs-java-lang/app/src/main/java/com/eomcs/oop/ex06/d/Exam0110.java
@@ -56,8 +56,8 @@ public class Exam0110 {
 
     System.out.println("----------------------");
 
-    A a4 = new A3();
-    a4.m(); // A2의 m() 호출
+    A obj2 = new A3();
+    obj2.m(); // A2의 m() 호출
     // a4가 실제 가리키는 A3 클래스부터 상위 클래스로 따라 올라가면서
     // 첫 번째로 만난 m()을 호출한다.
     System.out.println("--------------------");
