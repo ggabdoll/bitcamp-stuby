1ade54ab64cb46f42ab1e494744db646749bdf06
ggabdoll <ggabdoll0327@naver.com>
2022-01-17 12:22:36 +0900

    Interface

--- mylist-boot/app/src/main/java/com/eomcs/mylist/App.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/App.java
@@ -1,14 +1,13 @@
 package com.eomcs.mylist;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-@EnableAutoConfiguration
+
 public class App {
 
   public static void main(String[] args) {
