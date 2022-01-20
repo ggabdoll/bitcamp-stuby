ca1ef2b7bdcd48afe1f765803cd45b65db2a17f2
ggabdoll <ggabdoll0327@naver.com>
2022-01-20 11:44:00 +0900

    Serialize(transient)

--- eomcs-java-lang/app/src/main/java/com/eomcs/io/ex11/e/Exam0510.java
+++ eomcs-java-lang/app/src/main/java/com/eomcs/io/ex11/e/Exam0510.java
@@ -1,4 +1,7 @@
 // 인스턴스 입출력 - 'transient' modifier
+// modifier : 클래스, 변수, 메서드의 선언부에 함께 사용되어 부가적인 의미를 부여한다.
+//              1 접근 제어자 : public, protected, default, private
+//              2 그외 제어자 : static, final, abstract, native, transient, synchronized, volatile, strictfp
 package com.eomcs.io.ex11.e;
 
 import java.io.FileOutputStream;
@@ -17,6 +20,7 @@ public class Exam0510 {
     s.math = 92;
     s.compute();
 
+    System.out.println(s);
     // serialize 대상에서 제외된 필드(transient가 붙은 필드)는 
     // 다음과 같이 임의로 값을 설정해봐야 소용없다.
     s.sum = 300;
