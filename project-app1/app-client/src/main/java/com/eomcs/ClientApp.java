399b41eb31c6391b13063a9d88854c9715c73a99
ggabdoll <ggabdoll0327@naver.com>
2022-01-27 15:31:29 +0900

    aws test

--- project-app1/app-client/src/main/java/com/eomcs/ClientApp.java
+++ project-app1/app-client/src/main/java/com/eomcs/ClientApp.java
@@ -1,6 +1,8 @@
 package com.eomcs;
 
+import java.io.PrintStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ClientApp {
 
@@ -9,8 +11,23 @@ public class ClientApp {
     System.out.println("[계산기 클라이언트]");
 
     //1) 서버애플리케이션과 네트워크 연결을 수행한다. 
-    Socket socket = new Socket("127.0.0.1", 8888); // 서버와 연결될 때까지 객체를 생성하지 않는다.
+    Socket socket = new Socket("localhost", 8888); // 서버와 연결될 때까지 객체를 생성하지 않는다.
     System.out.println("서버와 연결되었음!");
+
+    // 데이터를 주고 받기 위한 입출력 스트림을 준비한다. 
+    PrintStream out = new PrintStream(socket.getOutputStream());
+    Scanner in = new Scanner(socket.getInputStream());
+
+    // 서버에 데이터를 보낸다.
+    out.println("이상준 사랑해");
+
+    // 서버가 응답한 데이터를 읽는다. 
+    String response = in.nextLine();
+    System.out.println("==>" + response);
+
+    // 입출력 도구를 다 사용했으면 자원을 해제한다. 
+    out.close();
+    in.close();
 
     // 서버 애플리케이션과 연결된 것을 끊는다. 
     socket.close();
