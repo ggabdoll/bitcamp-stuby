df34c244674f55ecf8605c9335e6782582820608
ggabdoll <ggabdoll0327@naver.com>
2022-02-09 18:05:09 +0900

    multi Thread

--- eomcs-java-lang/app/src/main/java/com/eomcs/net/ex12/swing/ChatClient.java
+++ eomcs-java-lang/app/src/main/java/com/eomcs/net/ex12/swing/ChatClient.java
@@ -6,16 +6,17 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.Socket;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
-import com.eomcs.io.ex08.DataInputStream;
 
 public class ChatClient extends JFrame{
   private static final long serialVersionUID = 1L;
@@ -31,7 +32,7 @@ public class ChatClient extends JFrame{
   JTextArea messageListTa = new JTextArea();
   public ChatClient() {
 
-    super("계산기");
+    super("채팅할거다");
     addWindowListener(new WindowAdapter() {
       @Override
       public void windowClosing(WindowEvent e) {
@@ -48,23 +49,26 @@ public class ChatClient extends JFrame{
     topPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
     topPanel.add(addressTF);
     topPanel.add(portTF);
+
     JButton connectBtn = new JButton("연결");
     connectBtn.addActionListener(this::connectChatServer);
     topPanel.add(connectBtn);
     contentPane.add(topPanel, BorderLayout.NORTH);
+    portTF.addActionListener(this::connectChatServer);
 
-
-    contentPane.add(messageListTa, BorderLayout.CENTER);
+    JScrollPane scrollPane = new JScrollPane(messageListTa);
+    contentPane.add(scrollPane, BorderLayout.CENTER);
 
 
     JPanel bottomPanel = new JPanel();
     bottomPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
     bottomPanel.add(messageTf);
     JButton sendBtn = new JButton("보내기");
-    sendBtn.addActionListener(this::connectChatServer);
+    sendBtn.addActionListener(this::sendMessage);
     bottomPanel.add(sendBtn);
     contentPane.add(bottomPanel, BorderLayout.SOUTH);
 
+    messageTf.addActionListener(this::sendMessage);
 
     setVisible(true);
   }
@@ -87,19 +91,45 @@ public class ChatClient extends JFrame{
       in = new DataInputStream(sk.getInputStream());
       out = new DataOutputStream(sk.getOutputStream());
 
+      new MessageReceiver(in).start();
+
     } catch (Exception ex) {
       JOptionPane.showMessageDialog(this, "서버에 연결중 오류발생!", "실행 오류!", JOptionPane.ERROR_MESSAGE);
     }
   }
 
-  public void SednMessage(ActionEvent e) {
-    System.out.println("메시지 보내기");
+  public void sendMessage(ActionEvent e) {
+    if (messageTf.getText().length() == 0) {
+      return;
+    }
     try {
       out.writeUTF(messageTf.getText());
+      out.flush();
+      messageTf.setText("");
+
     } catch (Exception ex) {
       JOptionPane.showMessageDialog(this, "메세지 전송 오류!", "통신오류!", JOptionPane.ERROR_MESSAGE);
     }
-
   }
 
+  class MessageReceiver extends Thread {
+
+    DataInputStream in;
+
+    public MessageReceiver(DataInputStream in) {
+      this.in = in;
+    }
+
+    @Override
+    public void run() {
+      while(true) {
+        try {
+          String message = in.readUTF();
+          messageListTa.append(message + "\n");
+
+        } catch(Exception e) {
+        }
+      }
+    }
+  }
 }
