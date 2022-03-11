605819e4d2ab911771e666d52c2f4cb32c03360d
ggabdoll <ggabdoll0327@naver.com>
2022-03-11 16:39:39 +0900

    properties

--- mylist-boot/app/src/main/java/com/eomcs/mylist/App.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/App.java
@@ -1,10 +1,13 @@
 package com.eomcs.mylist;
 
 import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +20,28 @@ public class App {
     SpringApplication.run(App.class, args);
   }
 
-  // DB커넥션 풀 준비 
-  public static DataSource dataSource;
+  // Spring 프레임워크(IoC = 객체풀)에서 객체를 생성한 후 보관하도록 만드는 방법
+  @Bean 
+  // => 스프링 부트를 시작할때 
+  // => 이 메서드가 리턴한 값은 스프링 부트의 객체풀(object poll)에 보관한다.
+  public DataSource createDataSource(
+      @Value("${spring.datasource.driver-class-name}") String driverClassName, 
+      @Value("${spring.datasource.url}") String url, 
+      @Value("${spring.datasource.username}") String username, 
+      @Value("${spring.datasource.password}") String password
+      ) {
+    try {
+      DriverManagerDataSource connectionPool = new DriverManagerDataSource();
+      connectionPool.setDriverClassName(driverClassName);
+      connectionPool.setUrl(url);
+      connectionPool.setUsername(username);
+      connectionPool.setPassword(password);
 
+      return connectionPool;
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
   //@Bean
   public CommandLineRunner commandLineRunner(ApplicationContext beanContainer) {
     return args -> {
