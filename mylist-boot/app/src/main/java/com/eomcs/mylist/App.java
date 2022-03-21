9690f7dce15c25027fee036ed8a880ab381ef627
ggabdoll <ggabdoll0327@naver.com>
2022-03-21 11:23:07 +0900

    transactionManager

--- mylist-boot/app/src/main/java/com/eomcs/mylist/App.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/App.java
@@ -1,17 +1,24 @@
 package com.eomcs.mylist;
 
+import javax.sql.DataSource;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-@EnableTransactionManagement
+@EnableTransactionManagement //애노테이션으로 트랙젝션을 사용할 수 있게 한다.
 
 public class App {
+
+  PlatformTransactionManager trasactionManager(DataSource ds) {
+    return new DataSourceTransactionManager(ds);
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(App.class, args);
--- mylist-boot/app/src/main/java/com/eomcs/mylist/controller/ContactController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/controller/ContactController.java
@@ -2,6 +2,9 @@ package com.eomcs.mylist.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.eomcs.mylist.dao.ContactDao;
@@ -14,6 +17,9 @@ public class ContactController {
   @Autowired
   ContactDao contactDao;
 
+  @Autowired
+  TransactionTemplate trasactionTemlplate;
+
   @RequestMapping("/contact/list")
   public Object list() {
     List<Contact> contacts = contactDao.findAll();
@@ -25,15 +31,28 @@ public class ContactController {
 
   @RequestMapping("/contact/add")
   public Object add(Contact contact, String[] tel) throws Exception {
-    contactDao.insert(contact);
-    for (int i = 0; i < tel.length; i++) {
-      String[] value = tel[i].split("_");
-      if (value[1].length() == 0) {
-        continue;
+
+
+    //1) 트랙잭션으로 묶어서 실행할 작업을 정의
+    // => 스프링 프레임우크에서 정한 규칙에 따라 정의해야 한다.
+    class ContactAddTransaction implements TransactionCallback {
+      @Override
+      public Object doInTransaction(TransactionStatus status) {
+        // 트랜잭션으로 묶어서 할 작업을 기술한다.
+        contactDao.insert(contact);
+        for (int i = 0; i < tel.length; i++) {
+          String[] value = tel[i].split("_");
+          if (value[1].length() == 0) {
+            continue;
+          }
+          contactDao.insertTel(new ContactTel(contact.getNo(), Integer.parseInt(value[0]), value[1]));
+        }
+        return 1;
       }
-      contactDao.insertTel(new ContactTel(contact.getNo(), Integer.parseInt(value[0]), value[1]));
     }
-    return 1;
+
+    //2) 트랜잭션 작업을 수행한다.
+    return trasactionTemlplate.execute(new ContactAddTransaction());
   }
 
   @RequestMapping("/contact/get")
